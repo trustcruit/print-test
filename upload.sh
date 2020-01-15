a40507d7228b6aa8c229b1fd17a1afaa16777508
Sven Parker <Knogobert@users.noreply.github.com>
2020-01-15 17:12:34 +0100

    Activated S3 upload script

--- upload.sh
+++ upload.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-# Commented out until needed
-# aws s3 sync . s3://print-test --acl public-read --exclude "*.map" --exclude ".*" --exclude "*.scss" --exclude "*.md" --exclude "*.sh" --delete
+aws s3 sync . s3://tc-print-test --acl public-read --exclude "*.map" --exclude ".*" --exclude "*.scss" --exclude "*.md" --exclude "*.sh" --delete
