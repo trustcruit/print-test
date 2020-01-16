4f7b9c6b9033886b6d9a719b7fe6309f448db669
Niels Lemmens <draso.odin@gmail.com>
2020-01-16 11:23:45 +0100

    Update upload.sh to correct bucket

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-aws s3 sync . s3://tc-print-test --acl public-read --exclude "*.map" --exclude ".*" --exclude "*.scss" --exclude "*.md" --exclude "*.sh" --delete
+aws s3 sync . s3://sprancher-dashboard2/dashboard/test/ --acl public-read --exclude "*.map" --exclude ".*" --exclude "*.scss" --exclude "*.md" --exclude "*.sh" --delete
